2cd26488389451ad971b2eaa107bdce08776dd58
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2020-11-06 19:45:46 +0700

    delete last project, begin work with new project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log('hello world');
+const menuBtn = document.querySelector('.menu-btn');
+const hamburderBtn = document.querySelector('.menu-btn-burger');
+const nav = document.querySelector("nav")
+const menuNav = document.querySelector(".menu-nav");
+const menuNavItems = document.querySelectorAll('.menu-nav-item');
+
+
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenuBtn);
+
+function toggleMenuBtn(){
+    if(!showMenu){
+        hamburderBtn.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        menuNavItems.forEach(el =>{
+            el.classList.add('open');
+        });
+        
+        showMenu = true;
+    }else{
+        hamburderBtn.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        menuNavItems.forEach(el => {
+            el.classList.remove('open');
+        });
+
+        showMenu = false;
+    }
+}
